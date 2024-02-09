0257eac78d5abd79603264be3891a066ea3dab8d
noornabi07 <noornabiprogram07@gmail.com>
2024-02-09 22:56:47 +0600

    add user form make design done

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import Container from '../Container/Container';
 
 const AddUser = () => {
     return (
-        <div>
-            <h2>Submit Your New User Here</h2>
-        </div>
+        <Container>
+            <div className=' px-10 py-5 mt-8 bg-gray-200 rounded-xl'>
+                <form onSubmit={''}>
+                    {/* reciepe name input */}
+                    <div className='md:flex items-center justify-between gap-5'>
+                        <div className="form-control w-full md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-semibold text-green-700">First Name*</span>
+                            </label>
+                            <input type="text" placeholder="First name" className="input input-bordered w-full " />
+                        </div>
+                        <div className="form-control w-full md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-semibold text-green-700">First Name*</span>
+                            </label>
+                            <input type="text" placeholder="First name" className="input input-bordered w-full " />
+                        </div>
+                    </div>
+
+                    <div className='md:flex items-center justify-between gap-5'>
+                        <div className="form-control w-full md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-semibold text-green-700">Email*</span>
+                            </label>
+                            <input type="email" placeholder="First name" className="input input-bordered w-full " />
+                        </div>
+                        <div className="form-control w-full md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-semibold text-green-700">Address*</span>
+                            </label>
+                            <input type="text" placeholder="First name" className="input input-bordered w-full " />
+                        </div>
+                    </div>
+
+                    <div className='md:flex items-center justify-between gap-5'>
+                        <div className="form-control w-full md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-semibold text-green-700">Company Name*</span>
+                            </label>
+                            <input type="email" placeholder="company name" className="input input-bordered w-full " />
+                        </div>
+                        <div className="form-control w-full md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-semibold text-green-700">User Image*</span>
+                            </label>
+                            <input type="file" className="file-input file-input-bordered w-full " />
+                        </div>
+                    </div>
+                    
+                    <div className='text-center'>
+                        <input className='btn text-white font-semibold text-lg mt-4 text-center btn-wide bg-green-700 border-none' type="submit" value="Add New Item" />
+                    </div>
+                </form>
+            </div>
+        </Container>
     );
 };
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div className='mt-10'>
+        <div className='mt-5'>
             <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
                 <nav className="grid grid-flow-col gap-4">
                     <a className="link link-hover">About us</a>
